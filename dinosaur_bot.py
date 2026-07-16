a469772d0b1d9a75e2d4f887009e74ae9bf9186e
USER <eugenemabansag8@gmail.com>
2026-07-16 12:42:26 +0800

    Added function to press jump after it detects a cactus

--- dinosaur_bot.py
+++ dinosaur_bot.py
@@ -1,17 +1,18 @@
 import pyautogui as pya
 import time as ti
+import os
 
 class MainApp:
     def __init__(self):
         pya.FAILSAFE = True
         pya.PAUSE = 0.5
-        self.screen_width, self.screen_height = pya.size()
-        print(self.screen_width, self.screen_height)
 
         self.initial_area_x, self.initial_area_y = 1710, 400
         self.final_area_x, self.final_area_y = 1860, 490
 
         self.cactus_color_r, self.cactus_color_g, self.cactus_color_b = 106, 160, 69
+
+        self.region = (self.initial_area_x, self.initial_area_y, (self.final_area_x - self.initial_area_x), (self.final_area_y - self.initial_area_y))
 
         pya.moveTo(1300, 400, duration = 0.1)
 
@@ -29,12 +30,24 @@ class MainApp:
             pixel_color = pya.pixel(position_x, position_y)
             print(f"Mouse color at: {pixel_color}")
 
-    def detect_cactus(self):
-        region = (self.initial_area_x, self.initial_area_y, (self.final_area_x - self.initial_area_x), (self.final_area_y - self.initial_area_y))
-        screenshot = pya.screenshot(region = region)
+    def detect_cactus(self, images):
+        screenshot = pya.screenshot(region = self.region)
         screenshot.save('region_captured.png')
+
+        for image in images:
+            try:
+                locate = pya.locate(image, screenshot, confidence = 0.6)
+                if locate:
+                    print("Cactus!")
+                    ti.sleep(1.5)
+                    pya.press('space')
+                    ti.sleep(0.1)
+                    pya.press('down')
+            except pya.ImageNotFoundException:
+                continue
 
 if __name__ == "__main__":
     main = MainApp()
+    image_list = ['cactus.png','cactus_2.png','cactus_3.png','cactus_4.png']
     while True:
-        main.detect_cactus()
+            main.detect_cactus(image_list)
